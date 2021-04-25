8d43064e9fd76118706eaddc401167d88270f680
pdxbellasaurus <carrieannyoung@gmail.com>
2021-04-24 23:58:10 -0700

    removed commented lines

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,3 @@
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASS,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//       port: 3306
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
-
-
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
